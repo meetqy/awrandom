25c565e04236c5e66edb79945ee646478683cd25
meetqy <meetqy@icloud.com>
2025-10-17 11:32:29 +0800

    add mode toggle

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -10,7 +10,7 @@ export const Providers = ({ children, breadcrumb }: { children: ReactNode; bread
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
           {breadcrumb}
